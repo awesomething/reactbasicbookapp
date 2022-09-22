404bb52673e9f23c317387ec00efd924504e7df1
Henry Ebomah <henryebomah@gmail.com>
2022-09-22 18:19:03 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {data} from './books'
+import SpecificBook from './Book'
+import {greeting} from './testing/testing'
+
+function App() {
+  console.log(greeting);
+  return (
+    <section className='booklist'>
+      {data.map((book, index) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
+      })}
+    </section>
+  );
+}
+
+export default App
